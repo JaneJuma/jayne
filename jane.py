060ac81a5cde521fbebcda256110009282f804c2
JaneJuma <jaynejumah@gmail.com>
2018-06-02 10:58:55 +0300

    data structures

---
+++ jane.py
@@ -0,0 +1,15 @@
+first_list = [1, 2, 3, 4, 5]
+
+dress_list = [2, 'short']
+
+string_list = ['irene', 'mercy', 'jane', 'juma']
+
+halima_tuple = ('halima', 'abdalla', 'nasreen')
+
+fruit_set = ('oranges', 'mangoes', 'apples', 'grapes')
+
+my_dict = {'name':'jane', 'age':'12', 'nationality':'kenyan', 'address':[234]}
+
+print('first_list', 'dress_list', 'string_list', 'halima_tuple', 'fruit_set', 'my_dict')
+
+
